7f6839568858cb0eeccdeca338e80a10b9feef06
Armel Cloarec <armel@armelclo.fr>
2025-11-06 17:22:34 +0100

    add ldap ?

--- main.go
+++ main.go
@@ -14,28 +14,35 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
 
-	fmt.Println("Server listing to :8080")
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		return
-	}
-
+	var err error
 	l, err = ldap.DialURL("ldap://127.0.0.1:389")
 	if err != nil {
 		log.Fatal(err)
 	}
-	//defer l.Close()
+	defer l.Close()
 
+	fmt.Println(l)
 	fmt.Println("Binded !")
+
+	fmt.Println("Server listing to :8080")
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		return
+	}
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	err := l.Bind("cn=superadmin,ou=users,dc=yunohost,dc=org", os.Args[1])
+	err := l.Bind("uid=superadmin,ou=users,dc=yunohost,dc=org", os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = fmt.Fprintf(w, "Hello de Armel")
+	res, err := l.WhoAmI(nil)
+	if err != nil {
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "Hello de %v <br> %v", res.AuthzID, r.Header)
 	if err != nil {
 		return
 	}
